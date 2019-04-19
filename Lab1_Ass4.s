f7fe6adf603c181e8e01dcd6c0b0fd1909c8438d
antoniooreany <antonio_oreany@mail.ru>
2019-04-19 23:08:15 +0200

    the registers were put in proper order

--- Lab1_Ass4.s
+++ Lab1_Ass4.s
@@ -16,20 +16,20 @@ START:
 	mov r3, r0		# i=0
 	movi r4, 2		# c=2
 	
-	movia r6, addrArrayA	# r6 <- start address of ArrayA
-	movia r8, addrArrayB	# r8 <- start address of ArrayB
+	movia r5, addrArrayA	# r5 <- start address of ArrayA
+	movia r6, addrArrayB	# r6 <- start address of ArrayB
 	
 LOOP:
 	bge r3, r2, END		# if i>=addrArrayA.length => END
 	
-	ldw r5, (r6)		# read next element of ArrayA
+	ldw r7, (r5)		# read next element of ArrayA
 	
-	add r7, r5, r4		# addrArrayB = addrArrayA + r4
+	add r8, r7, r4		# addrArrayB = addrArrayA + r4
 
-	stw r7, (r8)		# store next element of ArrayB
+	stw r8, (r6)		# store next element of ArrayB
 	
+	addi r5, r5, 4		# increment address of ArrayA by 4
 	addi r6, r6, 4		# increment address of ArrayA by 4
-	addi r8, r8, 4		# increment address of ArrayA by 4
 	addi r3, r3, 1		# increment i by 1
 	
 	br LOOP			# continue LOOP
